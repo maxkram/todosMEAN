b92052f946879ebd1b8858a649fffaab07e405b3
maxkram <max.kramer@live.com>
2017-06-15 12:50:06 +0300

    added client

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,90 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://maxkram:ruslan@ds127982.mlab.com:27982/meantodos',['todos']);
 
 router.get('/todos', function(req, res, next){
-    res.send('TODOS API');
+    db.todos.find(function(err,todos){
+        if(err){
+            res.send(err);
+        }else{
+            res.json(todos);
+        }
+    });
+});
+
+router.get('/todo/:id', function(req, res, next){
+    db.todos.findOne({
+        _id:mongojs.ObjectId(req.params.id)
+    },function(err,todo){
+        if(err){
+            res.send(err);
+        }else{
+            res.json(todo);
+        }
+    });
+});
+
+router.post('/todo',function(req,res,next){
+    var todo = req.body;
+    if(!todo.text || !(todo.isCompleted + '')){
+        res.status(400);
+        res.json({
+            "error": "Données invalides"
+        });
+    }else{
+        db.save(todo,function(err,result){
+            if(err){
+                res.send(err);
+            }else{
+                res.json(result);
+            }            
+        });
+    }
+});
+
+router.put('/todo/:id',function(req,res,next){
+    var todo = req.body;
+    var updObj = {};
+
+    if(todo.isCompleted){
+        updObj.isCompleted = todo.isCompleted;
+    }
+
+    if(todo.text){
+        updObj.text = todo.text;
+    }
+
+    if(!updObj){
+        res.status(400);
+        res.json({
+            "error": "Données invalides"
+        });
+    } else {
+        db.todos.update({
+            _id: mongojs.ObjectId(req.params.id)
+        },updObj,{},function(err,result){
+            if(err){
+                res.send(err);
+            }else{
+                res.json(todo);
+            }
+
+        });
+    }
+});
+
+router.delete('/todo/:id',function(req,res,next){
+    db.todos.remove({
+        _id: mongojs.ObjectId(req.params.id)
+    },'',{},function(err,result){
+        if(err){
+            res.send(err);
+        }else{
+            res.json(todo);
+        }
+
+    });
 });
 
 module.exports = router;
